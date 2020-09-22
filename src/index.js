9259eb47fb8a81acc1c979373dc887a11f28d239
sahithya14 <sahithyareddy33@gmail.com>
2020-09-22 22:19:57 +0530

    combineReducers

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from "redux";
+import {createStore,combineReducers} from "redux";
 import registerServiceWorker from './registerServiceWorker';
-import reducer from "../src/store/reducer";
+import counterReducer from "../src/store/reducers/counterReducer";
+import resultReducer from "../src/store/reducers/resultReducer";
 import {Provider} from "react-redux";
 
-const reduxStore= createStore(reducer);
+const rootReducer = combineReducers({ctr:counterReducer,res:resultReducer})
+const reduxStore = createStore(rootReducer);
 ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
